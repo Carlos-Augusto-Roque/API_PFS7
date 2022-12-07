9e06b6ca09c79d0021f48d0c870ed384e4a7adfa
Carlos <roqueaugustocarlos@gmail.com>
2022-12-07 20:37:41 -0300

    conteúdo desenvolvido no encontro remoto 1

---
+++ ChapterAPI/ChapterAPI/Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using ChapterAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterAPI.Controllers
+{
+    [Produces("application/json")]//o formato de resposta é do tipo json
+    [Route("api/[controller]")] //api/livro
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livro)
+        {
+            _livroRepository = livro; 
+        }
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Ler());
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }        
+        }
+
+    }
+}
